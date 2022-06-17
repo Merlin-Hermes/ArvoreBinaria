af92c5bc98ba390e374f5bcdc883be5d1ccdbb64
Merlin-Hermes <tiago1haise@gmail.com>
2022-06-17 10:22:05 -0300

    atualizando estrutura, adicionando logica do problema.

--- src/arvoreBinaria/Laboratorio.java
+++ src/arvoreBinaria/Laboratorio.java
@@ -2,20 +2,56 @@ package arvoreBinaria;
 
 import arvoreBinaria.service.ArvoreBinaria;
 
+import java.util.Random;
+import java.util.Scanner;
+
 public class Laboratorio {
 
-	public static void main(String[] args) {
-		ArvoreBinaria arvore = new ArvoreBinaria();
+    public static void main(String[] args) {
+        ArvoreBinaria arvore = new ArvoreBinaria();
+        Scanner leitor = new Scanner(System.in);
+        Random cartas = new Random();
 
-		arvore.insereValor(arvore.getRaiz(), 5);
-		arvore.insereValor(arvore.getRaiz(), 10);
-		arvore.insereValor(arvore.getRaiz(), 15);
-		arvore.insereValor(arvore.getRaiz(), 20);
-		arvore.insereValor(arvore.getRaiz(), 25);
+        int dano = 0;
 
-		arvore.imprimeArvore(arvore.getRaiz());
-		arvore.liberaMemoria(arvore.getRaiz());
 
-	}
+        System.out.println("--------------------------------");
+        System.out.println("MARSHADOW");
+        System.out.println("--------------------------------");
+        System.out.println("Esta e a guerra final contra Marshadow");
+
+
+        for (int i = 0; i < 5; i++) {
+            arvore.insereValor(arvore.getRaiz(), cartas.nextInt(100) + 1);
+        }
+
+        for (int i = 0; i < 5; i++) {
+
+            System.out.println("Tente advinhar as cartas viradas para baixo");
+            int chute = leitor.nextInt();
+
+            boolean tentativa = arvore.buscarValor(arvore.getRaiz(), chute);
+
+            if (!tentativa) {
+                System.out.println("infelizmente deu ruim amigo");
+            } else {
+                System.out.println("Parabens voce acertou");
+                arvore.removeNo(arvore.getRaiz(), chute);
+                dano++;
+            }
+
+        }
+
+        if (dano < 3) {
+            System.out.println("Que pena voce perdeu, Marshadow e invencivel");
+        } else {
+            System.out.println("para bens você derrotou Marshadow");
+        }
+
+        System.out.println("cartas que não foram advinhadas: ");
+        arvore.imprimeArvore(arvore.getRaiz());
+        arvore.liberaMemoria(arvore.getRaiz());
+
+    }
 
 }
--- src/arvoreBinaria/service/ArvoreBinaria.java
+++ src/arvoreBinaria/service/ArvoreBinaria.java
@@ -3,32 +3,31 @@ package arvoreBinaria.service;
 import arvoreBinaria.entity.No;
 
 public class ArvoreBinaria {
-	No raiz = null;
+    No raiz = null;
 
-	public No getRaiz() {
-		return raiz;
-	}
+    public No getRaiz() {
+        return raiz;
+    }
 
-	public void liberaMemoria(No raiz) {
-		if (raiz == null || raiz.noFolha()) {
-			return;
-		}
+    public void liberaMemoria(No raiz) {
+        if (raiz == null || raiz.noFolha()) {
+            return;
+        }
 
-		if (raiz.getDireito() != null) {
-			liberaMemoria(raiz.getDireito());
-			raiz.setDireito(null);
-		}
+        if (raiz.getDireito() != null) {
+            liberaMemoria(raiz.getDireito());
+            raiz.setDireito(null);
+        }
 
-		if (raiz.getEsquerdo() != null) {
-			liberaMemoria(raiz.getEsquerdo());
-			raiz.setEsquerdo(null);
-		}
+        if (raiz.getEsquerdo() != null) {
+            liberaMemoria(raiz.getEsquerdo());
+            raiz.setEsquerdo(null);
+        }
 
-		this.raiz = null;
-	}
+        this.raiz = null;
+    }
 
-	void calculaNivelNos(No raiz, int nivel) {
-		// Verifica se existe a arvore
+	public void calculaNivelNos(No raiz, int nivel) {
 		if (raiz == null)
 			return;
 
@@ -37,48 +36,89 @@ public class ArvoreBinaria {
 		calculaNivelNos(raiz.getDireito(), nivel + 1);
 	}
 
+
 	public void insereValor(No raiz, int valor) {
-		// Verifica se a �rvore est� vazia
-		if (this.raiz == null) {
-			this.raiz = new No(valor);
-			return;
-		}
+        if (this.raiz == null) {
+            this.raiz = new No(valor);
+            return;
+        }
 
-		// Verifica para que lado da raiz vai andar
-		if (valor > raiz.getValor()) {
-			// Se nao tiver lado direito, insere
-			if (raiz.getDireito() == null) {
-				raiz.setDireito(new No(valor));
-				return;
-			}
+        if (valor > raiz.getValor()) {
+            if (raiz.getDireito() == null) {
+                raiz.setDireito(new No(valor));
+                return;
+            }
 
-			// Insere recursivamente
-			insereValor(raiz.getDireito(), valor);
-		} else {
-			if (raiz.getEsquerdo() == null) {
-				raiz.setEsquerdo(new No(valor));
-				return;
-			}
+            insereValor(raiz.getDireito(), valor);
+        } else {
+            if (raiz.getEsquerdo() == null) {
+                raiz.setEsquerdo(new No(valor));
+                return;
+            }
 
-			insereValor(raiz.getEsquerdo(), valor);
-		}
-	}
-	
-	public boolean buscarValor(int Valor) {
-		
-		return false;
-	}
+            insereValor(raiz.getEsquerdo(), valor);
+        }
+    }
 
-	public void removeNo(No raiz, int Valor) {
-		return;
-	}
-	
-	public void imprimeArvore(No raiz) {
-		if (raiz == null)
-			return;
+    public boolean buscarValor(No raiz, int Valor) {
+        boolean aux = false;
 
-		imprimeArvore(raiz.getEsquerdo());
-		System.out.println(raiz.getValor());
-		imprimeArvore(raiz.getDireito());
-	}
+        if (raiz == null) {
+            aux = false;
+        } else if (raiz.getValor() == Valor) {
+            aux = true;
+        } else {
+            if (raiz.getValor() > Valor) {
+                aux = buscarValor(raiz.getDireito(), Valor);
+            } else {
+                aux = buscarValor(raiz.getEsquerdo(), Valor);
+            }
+        }
+        return aux;
+    }
+
+    private No maiorValor(No no) {
+        while (no.getDireito() != null) {
+            no = no.getDireito();
+        }
+
+        return no;
+    }
+
+    public No removeNo(No raiz, int Valor) {
+        if (raiz == null) {
+            return raiz;
+        }
+
+        if (Valor < raiz.getValor()) {
+            raiz.setEsquerdo(removeNo(raiz.getEsquerdo(), Valor));
+        } else if (Valor > raiz.getValor()) {
+            raiz.setDireito(removeNo(raiz.getDireito(), Valor));
+        } else {
+
+            if (raiz.getEsquerdo() == null && raiz.getDireito() == null) {
+                return null;
+            } else if (raiz.getEsquerdo() != null && raiz.getEsquerdo() != null) {
+                No maiorAntecessor = maiorValor(raiz.getEsquerdo());
+
+                raiz.setValor(maiorAntecessor.getValor());
+
+                raiz.setEsquerdo(removeNo(raiz.getEsquerdo(), maiorAntecessor.getValor()));
+            } else {
+                raiz = (raiz.getEsquerdo() != null) ? raiz.getEsquerdo() : raiz.getDireito();
+            }
+        }
+
+        return raiz;
+    }
+
+
+    public void imprimeArvore(No raiz) {
+        if (raiz == null)
+            return;
+
+        imprimeArvore(raiz.getEsquerdo());
+        System.out.println(raiz.getValor());
+        imprimeArvore(raiz.getDireito());
+    }
 }
